5d69351508c3fd93144c4cbaa0750348a2a2be84
Kyle Stough <kstough00@gmail.com>
2020-08-13 21:17:43 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,8 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
  
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-doc.css(".headline-26OIBN")
+html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+ 
+puts doc
 
 
 
